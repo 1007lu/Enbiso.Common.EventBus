a311edb0b8a3f7c36f6123da21cc4ee8b5cee163
Faraj Farook <farajfarook@gmail.com>
2018-05-18 08:03:31 +1000

    Adding app extensions

---
+++ Enbiso.Common.EventBus/Config/AppBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Enbiso.Common.EventBus.Abstractions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Enbiso.Common.EventBus.Config
+{
+    public static class AppBuilderExtensions
+    {
+        public static void UseEventBus(this IApplicationBuilder app, Action<IEventBus> init)
+        {
+            var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+            eventBus.Initialize(() => init(eventBus));
+        }
+    }
+}
